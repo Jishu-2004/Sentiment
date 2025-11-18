df986721099a1202060a9f73dde22199ba68a4eb
Jishu-2004 <aishik.jishu2004@gmail.com>
2025-11-18 17:59:50 +0530

    Update Product_M.py

--- Transformers/Models/Product_M.py
+++ Transformers/Models/Product_M.py
@@ -1,1 +1,25 @@
-# Created By LORD 
+# Product_M.py
+import torch
+import torch.nn as nn
+from transformers import DistilBertModel
+
+class ProductModel(nn.Module):
+    def __init__(self, num_labels=3):
+        super(ProductModel, self).__init__()
+
+        self.encoder = DistilBertModel.from_pretrained("distilbert-base-uncased")
+
+        self.dropout = nn.Dropout(0.3)
+        self.classifier = nn.Linear(self.encoder.config.dim, num_labels)
+
+    def forward(self, input_ids, attention_mask):
+        outputs = self.encoder(
+            input_ids=input_ids,
+            attention_mask=attention_mask
+        )
+
+        cls_token = outputs.last_hidden_state[:, 0]  
+        cls_token = self.dropout(cls_token)
+
+        logits = self.classifier(cls_token)
+        return logits
